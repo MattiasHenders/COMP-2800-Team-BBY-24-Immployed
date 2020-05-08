1138a7161169bf27198bf0f83700d8a5fc2021f0
Shubhankar Sethi <shubhankar.sethi@outlook.com>
2020-05-08 11:45:27 -0700

    Landing Page

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// Redirecting to creating a new post
+function employerPage(){
+    window.location.href = "employer_login.html";
+}
+
+// Redirecting to creating a new post
+function employeePage(){
+    window.location.href = "employee_login.html";
+}
