05f5113773550b7957e1002a07f76e937e851ee4
Mohib H <h.mohib88@gmail.com>
2020-05-08 12:24:29 -0700

    Changed the index.html to include the about us page

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,8 @@ function employerPage(){
 function employeePage(){
     window.location.href = "employee_login.html";
 }
+
+// Redirecting to the about us page
+function aboutUs(){
+    window.location.href = "about_us.html";
+}
