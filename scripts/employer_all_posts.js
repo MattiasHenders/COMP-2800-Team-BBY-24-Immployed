6ce3ebad0238dda9c9d3e59b87ff1764b65e16fb
MattiasHenders <henders.mattias@gmail.com>
2020-05-12 09:57:59 -0700

    Starting employer all posts pages

--- scripts/employer_all_posts.js
+++ scripts/employer_all_posts.js
@@ -2,37 +2,83 @@ const storageRef = firebase.storage().ref();
 
 let userId = location.hash.substring(1);
 let profile = db.collection("employee_users").doc(userId);
-let isEditing = false;
-let imageUploaded = false;
 
 //Populate the profile page
-window.onload = function() {
+window.onload = function () {
     loadPosts();
 }
 
 //Loads database company logo
-function loadPosts(){
-    profilePicsStore = storageRef.child('profilePictures');
-    console.log(profilePicsStore);
+function loadPosts() {
+    logoPicsStores = storageRef.child('companyLogos');
+    console.log(logoPicsStores);
 
-    userPics = profilePicsStore.child(userId);
-    console.log(userPics);
+    logoPics = logoPicsStores.child(userId);
+    console.log(logoPics);
 
-    userProfilePic = userPics.child('profilePic.png');
+    userProfilePic = logoPics.child('logoPic.png');
     console.log(userProfilePic);
-    userProfilePic.getDownloadURL().then(function(url) {
+    userProfilePic.getDownloadURL().then(function (url) {
         var test = url;
         document.querySelector('img').src = test;
-    }).catch(function(error) {
+    }).catch(function (error) {
         console.log('Error');
     });
 
-    profile.get().then(function(doc) {
+    addPostsDOM()
+}
 
-        $("#profileComponents > div > p").each(function(){
-            if($(this).text() == "" || $(this).text() == "undefined") {
-                $(this).text("Nothing entered yet!");
+function addPostsDOM() {
+    let arrayOfPosts = [];
+    db.collection("job_posts").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            if (doc.data().ownerOfPost == profile) {
+                arrayOfPosts.push(doc.data());
             }
         })
+    }).then(function () {
+        for (i = 0; i < arrayOfPosts.length; i++) {
+            arrayOfPosts(i);
+
+            var tag = document.createElement("div");
+            tag.id = "posting" + i;
+            var element = document.getElementById("myPostings");
+            element.appendChild(tag);
+
+            var tag = document.createElement("h4");
+            tag.id = "postTitle";
+            var text = document.createTextNode(arrayOfPosts(i).title);
+            tag.appendChild(text);
+            var element = document.getElementById("posting" + i);
+            element.appendChild(tag);
+
+            var tag = document.createElement("div");
+            tag.id = "companyLogo";
+            var element = document.getElementById("posting" + i);
+            element.appendChild(tag);
+            
+            var tag = document.createElement("img");
+            companyLogoStore = storageRef.child('companyLogos');
+            userLogo = companyLogoStore.child(userId);
+            userLogo.getDownloadURL().then(function(url) {
+                var test = url;
+                document.querySelector('img').src = test;
+            }).catch(function(error) {
+                console.log('error');
+            });
+            var element = document.getElementById("companyLogo");
+            element.appendChild(tag);
+
+            /*
+            <div id="postingOne">
+                <h4 id="postTitle"></h4>
+                Image goes here
+                <div id="companyLogo">
+                    <img alt="My company Logo">
+                </div>
+                <p id="jobDescription"></p>
+            </div>
+             */
+        }
     })
 }
