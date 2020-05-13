88f9adc1712df4ce7a827e408a26bcce586bfad5
MattiasHenders <henders.mattias@gmail.com>
2020-05-13 10:03:57 -0700

    Homepage edits

--- scripts/employer_homepage.js
+++ scripts/employer_homepage.js
@@ -1,29 +1,37 @@
 // Scroll to the top feature
-    mybutton = document.getElementById("top");
-    document.getElementById("employerProfileButton").onclick = employerHomePage;
+//mybutton = document.getElementById("top");
+$('document').ready(function () {
+    document.getElementById("employerProfileButton").onclick = employerProfilePage;
+});
 
-    function scrollFunction() {
+function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 10) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
-    }
+}
 
-    function topFunction() {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 // Redirecting to creating a new post
-    function createNew(){
-        window.location.href = "employer_jobpost.html";
-    }
-
+function createNew() {
+    window.location.href = "employer_jobpost.html";
+}
 
 /**
  * Change to employer profile page
  */
-function employerHomePage(){
-    window.location.href = "employer_profile.html";
+function employerProfilePage() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            window.location.href = "employer_profile.html#" + user.uid;
+            console.log(user.uid);
+        } else {
+            console.log("Error, not logged in");
+        }
+    });
 }
