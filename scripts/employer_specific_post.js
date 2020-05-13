5d2e80a3db501286d964c758f2e0cf6e8a0e18e6
MattiasHenders <henders.mattias@gmail.com>
2020-05-13 15:29:35 -0700

    Posts for specific posts for employers

--- scripts/employer_specific_post.js
+++ scripts/employer_specific_post.js
@@ -5,5 +5,65 @@ window.onload = function() {
     profile.get().then(function(doc) {
 
         document.getElementById("postTitle").innerHTML = doc.data().title;
+        document.getElementById("postDescription").innerHTML = doc.data().description;
+        document.getElementById("postAddress").innerHTML = doc.data().address;
+        document.getElementById("postCity").innerHTML = doc.data().city;
+        document.getElementById("postProvince").innerHTML = doc.data().province;
+        document.getElementById("postPostalCode").innerHTML = doc.data().inputPostalCode;
+        document.getElementById("payAmount").innerHTML = doc.data().payAmount;
+        document.getElementById("payNegotiable").innerHTML = doc.data().payNegotiable;
+    })
+}
+
+/**
+ * Adds posts dynamically to the DOM
+ */
+function addPostsDOM() {
+
+    db.collection("job_posts").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            if (doc.data().ownerOfPost == profile) {
+                arrayOfPosts.push(doc.data());
+                arrayOfIDs.push(doc.id);
+            }
+        })
+
+    }).then(function () {
+        for (i = 0; i < arrayOfPosts.length; i++) {
+
+            var tag = document.createElement("div");
+            tag.id = "applicant" + i;
+            tag.class = "applicant";
+            var element = document.getElementById("applicantsSection");
+            element.appendChild(tag);
+
+            var tag = document.createElement("h4");
+            tag.id = "applicantName";
+            var text = document.createTextNode(arrayOfPosts[i].title);
+            tag.appendChild(text);
+            var element = document.getElementById("applicant" + i);
+            element.appendChild(tag);
+
+            var tag = document.createElement("p");
+            tag.id = "applicantDescription";
+            var text = document.createTextNode(arrayOfPosts[i].description);
+            tag.appendChild(text);
+            var element = document.getElementById("applicant" + i);
+            element.appendChild(tag);
+
+            var tag = document.createElement("button");
+            tag.id = "applicant" + i;
+            tag.class = "postButton";
+            var text = document.createTextNode("View Resume");
+            tag.appendChild(text);
+            var element = document.getElementById("applicant" + i);
+            element.appendChild(tag);
+
+            let test = arrayOfIDs[i];
+
+            tag.onclick = function(){
+                window.location = "employer_specific_post.html#" + test;
+            }
+        }
     })
 }
