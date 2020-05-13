e32c8e84d9540c602d7255b2c22b86e309af1df0
MattiasHenders <henders.mattias@gmail.com>
2020-05-13 09:38:01 -0700

    HTML, CSS, JavaScript added so user can view recent job posts

--- scripts/employer_all_posts.js
+++ scripts/employer_all_posts.js
@@ -1,84 +1,67 @@
-const storageRef = firebase.storage().ref();
+//let profile = db.collection("employee_users").doc(userId);
 
-let userId = location.hash.substring(1);
-let profile = db.collection("employee_users").doc(userId);
-
-//Populate the profile page
+/**
+ * Populate the profile page
+ */
 window.onload = function () {
-    loadPosts();
+    addPostsDOM();
 }
 
-//Loads database company logo
-function loadPosts() {
-    logoPicsStores = storageRef.child('companyLogos');
-    console.log(logoPicsStores);
-
-    logoPics = logoPicsStores.child(userId);
-    console.log(logoPics);
-
-    userProfilePic = logoPics.child('logoPic.png');
-    console.log(userProfilePic);
-    userProfilePic.getDownloadURL().then(function (url) {
-        var test = url;
-        document.querySelector('img').src = test;
-    }).catch(function (error) {
-        console.log('Error');
-    });
-
-    addPostsDOM()
-}
-
+/**
+ * Adds posts dynamically to the DOM
+ */
 function addPostsDOM() {
     let arrayOfPosts = [];
+    let arrayOfID = [];
+
     db.collection("job_posts").get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
+            arrayOfPosts.push(doc.data());
+            arrayOfID.push(doc.id);
             if (doc.data().ownerOfPost == profile) {
                 arrayOfPosts.push(doc.data());
             }
         })
     }).then(function () {
         for (i = 0; i < arrayOfPosts.length; i++) {
-            arrayOfPosts(i);
 
             var tag = document.createElement("div");
             tag.id = "posting" + i;
+            tag.class = "posting";
             var element = document.getElementById("myPostings");
             element.appendChild(tag);
-
+            
             var tag = document.createElement("h4");
             tag.id = "postTitle";
-            var text = document.createTextNode(arrayOfPosts(i).title);
+            var text = document.createTextNode(arrayOfPosts[i].title);
             tag.appendChild(text);
             var element = document.getElementById("posting" + i);
             element.appendChild(tag);
 
-            var tag = document.createElement("div");
-            tag.id = "companyLogo";
+            var tag = document.createElement("p");
+            tag.id = "postDescription";
+            var text = document.createTextNode(arrayOfPosts[i].description);
+            tag.appendChild(text);
+            var element = document.getElementById("posting" + i);
+            element.appendChild(tag);  
+            
+            var tag = document.createElement("button");
+            tag.id = "postButton" + i;
+            tag.class = "postButton";
+            var text = document.createTextNode("View Post");
+            tag.appendChild(text);
+            tag.onclick = jobPageTransfer(arrayOfID[i]);
             var element = document.getElementById("posting" + i);
             element.appendChild(tag);
-            
-            var tag = document.createElement("img");
-            companyLogoStore = storageRef.child('companyLogos');
-            userLogo = companyLogoStore.child(userId);
-            userLogo.getDownloadURL().then(function(url) {
-                var test = url;
-                document.querySelector('img').src = test;
-            }).catch(function(error) {
-                console.log('error');
-            });
-            var element = document.getElementById("companyLogo");
-            element.appendChild(tag);
-
-            /*
-            <div id="postingOne">
-                <h4 id="postTitle"></h4>
-                Image goes here
-                <div id="companyLogo">
-                    <img alt="My company Logo">
-                </div>
-                <p id="jobDescription"></p>
-            </div>
-             */
         }
     })
+}
+
+/**
+ * Change to specific job page
+ * @param {*} id id of job post
+ */
+function jobPageTransfer(id){
+    localStorage.setItem(0, id);
+    window.location = 'employer_specific_post.html';
 }
