39556594e331286b12f318a27c29fe73679fb69b
Shubhankar Sethi <shubhankar.sethi@outlook.com>
2020-05-15 11:55:08 -0700

    fixed employee homepage

--- scripts/employee_homepage.js
+++ scripts/employee_homepage.js
@@ -1,14 +1,10 @@
 // Redirecting to creating a new post
 function search(){
-    window.location.href = "";
+    window.location.href = "browse_jobs.html";
 }
 
 function editProfile(){
-    window.location.href = "";
-}
-
-function viewAll(){
-    window.location.href = "employee_allJobs.html";
+    window.location.href = "employee_profile.html";
 }
 
 // Easter Egg
