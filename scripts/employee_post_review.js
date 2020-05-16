1846f25278f5e87703a28ed3adbd44e71c6b71fa
MattiasHenders <henders.mattias@gmail.com>
2020-05-16 14:23:31 -0700

    Stylized buttons Apply to post button works

--- scripts/employee_post_review.js
+++ scripts/employee_post_review.js
@@ -8,8 +8,9 @@ let applicantLocation = [];
 window.onload = function () {
 
     jobPost.get().then(function (doc) {
-    
+
         applicantIDs = doc.data().applicants;
+        document.getElementById("applyButton").onclick = applyToPost;
 
         document.getElementById("postTitle").innerHTML = doc.data().title;
         document.getElementById("postDescription").innerHTML = doc.data().description;
@@ -19,5 +20,21 @@ window.onload = function () {
         document.getElementById("postPostalCode").innerHTML = doc.data().inputPostalCode;
         document.getElementById("payAmount").innerHTML = doc.data().payAmount;
         document.getElementById("payNegotiable").innerHTML = doc.data().payNegotiable;
+    });
+}
+
+/**
+ * Adds the employee to the list of users that applied for the job
+ */
+function applyToPost() {
+
+    let userID;
+
+    firebase.auth().onAuthStateChanged((user) => {
+        userID = user.uid;
+
+        jobPost.update( {
+            applicants: firebase.firestore.FieldValue.arrayUnion(userID)
+         });
     });
 }
